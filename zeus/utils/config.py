d8923bfbf87605abcf0f345295d6f8c47bed4a5a
vasilisvyth-orpheus <vasilis@orpheus-ai.nl>
2026-07-08 13:09:07 +0200

    reduce burn percent to 0.3

--- zeus/utils/config.py
+++ zeus/utils/config.py
@@ -207,7 +207,7 @@ def add_validator_args(cls, parser):
         "--neuron.burn_percent",
         type=float,
         help="The percentage of the reward to burn.",
-        default=0.4,
+        default=0.3,
     )
 
     parser.add_argument(
